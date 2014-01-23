4bedfabcb556c6720ae139dcb4249f5d1e63fd50
Siegfried Kiermayer <siegfried.kiermayer@mayflower.de>
2014-01-23 21:26:21 +0100

    initial vagrant, puppet and symfony import

---
+++ manifests/default.pp
@@ -0,0 +1,16 @@
+include '::mysql::server'
+
+exec { "apt-get update":
+  path => "/usr/bin",
+}
+
+package { "apache2":
+  ensure => present,
+  require => Exec["apt-get update"],
+}
+
+service { "apache2":
+  ensure => "running",
+  require => Package["apache2"],
+}
+
