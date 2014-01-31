c8216ece17a23e1da68bc10f05538fa10c8b45d5
Alberto Assmann <newloki91@googlemail.com>
2014-01-31 11:10:40 +0100

    fix apt get update exec order

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,15 +1,27 @@
 include '::mysql::server'
 
+Exec["apt-get update"] -> Package <| |>
+
 file { '/var/www/app':
   ensure => directory,
   recurse => true
+}
+
+exec { "apt-get update":
+  path => "/usr/bin",
 }
 
 class { 'apache' : }
 apache::vhost { 'dashi' :
    port => '80', 
    docroot => '/var/www/app/web',
+   docroot_owner => 'www-data',
+   docroot_group => 'www-data'
 }
 
-class { 'php' : }
+class { 'php' :
+    service => 'apache2',
+    require => Service['apache']
+}
+
 package {'php5-mysql': ensure => present }
