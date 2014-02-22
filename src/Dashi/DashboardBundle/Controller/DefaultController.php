31a2cfe1a4cb0e12d3d765a43924a00b4208887d
Siegfried Kiermayer <sicaine@gmail.com>
2014-02-22 21:21:06 +0100

    added first form for dashboards and persisting

--- src/Dashi/DashboardBundle/Controller/DefaultController.php
+++ src/Dashi/DashboardBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Dashi\DashboardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Dashi\DashboardBundle\Entity\Dashboard;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -16,5 +18,27 @@ class DefaultController extends Controller
     	$em = $this->getDoctrine()->getManager();
     	$dashboards = $em->getRepository('Dashi\DashboardBundle\Entity\Dashboard')->findAll();
     	return $this->render('DashiDashboardBundle:Default:manage.html.twig', array('dashboards' => $dashboards));
+    }
+    
+    public function newAction(Request $request)
+    {
+    	$dashboard = new Dashboard();
+    	$form = $this->createFormBuilder($dashboard)
+    	->add('name', 'text')
+    	->add('submit', 'submit')
+    	->getForm();
+    	
+    	$form->handleRequest($request);
+    	 
+    	if ($form->isValid()) {
+    		$em = $this->getDoctrine()->getManager();
+    		$em->persist($form->getData());
+    		$em->flush();
+    		return $this->redirect($this->generateUrl('dashi_dashboard_overview'));
+    	}
+    	
+    	return $this->render('DashiDashboardBundle:Default:new_dashboard.html.twig', array(
+    			'form' => $form->createView(),
+    	));
     }
 }
