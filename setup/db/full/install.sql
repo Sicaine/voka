96764869e57ff20992ecedd7e499e83b026eab5c
Siegfried <siegfried.kiermayer@mayflower.de>
2014-03-01 19:23:40 +0100

    updateded install script to reflect new widget

--- setup/db/full/install.sql
+++ setup/db/full/install.sql
@@ -1,5 +1,15 @@
-CREATE TABLE dashboard (
+ CREATE TABLE IF NOT EXISTS dashboard (
   id int unsigned AUTO_INCREMENT NOT NULL,
   name varchar (255) NOT NULL,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE widget (
+  id int unsigned AUTO_INCREMENT NOT NULL,
+  dashboard_id int unsigned,
+  xCord int unsigned,
+  yCord int unsigned,
+  width int unsigned,
+  height int unsigned,
+  PRIMARY KEY (id)
+)
