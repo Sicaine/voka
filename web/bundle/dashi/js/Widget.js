e3ba837597f4a0690757cfae2969096b2ea3f83b
Siegfried Kiermayer <siegfried.kiermayer@mayflower.de>
2014-03-14 15:24:32 +0100

    added dragAndDrop with close and style

--- web/bundle/dashi/js/Widget.js
+++ web/bundle/dashi/js/Widget.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('Library', []).
-    directive('widget', function() {
+    directive('widget', function($document) {
 
         console.log('widget global');
         return {
@@ -10,22 +10,75 @@ angular.module('Library', []).
 
             link: function(scope, element, $linkAttributes) {
 
-                element.bind('click', function(event) {
+                }
+
+
+
+                element.find('.name').bind('click', function(event) {
                     alert('clicked in widget');
                     event.stopPropagation();
                 });
 
+                element.find('.closeX').bind('click', function(event) {
+                    scope.$destroy();
+                    element.remove();
+                    event.stopPropagation();
+                });
 
+                scope.offsetX = event.offsetX;
+                scope.offsetY = event.offsetY;
+
+               // if user doesn't click on canvas, recalculate offset
+                var canvasElement = angular.element('#canvas');
+
+                if(canvasElement[0] != element[0] && false){
+                    scope.offsetX = scope.offsetX - (element[0].getBoundingClientRect().top - canvasElement[0].getBoundingClientRect().top);
+                    scope.offsetY = scope.offsetY - (element[0].getBoundingClientRect().left - canvasElement[0].getBoundingClientRect().left);
+                }
 
                 element.addClass('widget');
-                element.css('top', event.offsetY);
-                element.css('left', event.offsetX);
+                element.css('top', scope.offsetY);
+                element.css('left', scope.offsetX);
                 element.css('width', 300);
                 element.css('height', 100);
 
                 scope.$apply();
 
                 console.log('widget link');
+
+                element.find('.dragbar').on('mousedown', function(event) {
+                    var xElement = element.find('.closeX');
+                    if(xElement[0] == event.target[0]){
+                        return;
+                    }
+
+                    // Prevent default dragging of selected content
+                    event.preventDefault();
+                    scope.startX = event.pageX - scope.offsetX;
+                    scope.startY = event.pageY - scope.offsetY;
+                    $document.on('mousemove', mousemove);
+                    $document.on('mouseup', mouseup);
+                    event.stopPropagation();
+                });
+
+                function mousemove(event) {
+                    element.removeClass('saved');
+                    scope.offsetY = event.pageY - scope.startY;
+                    scope.offsetX = event.pageX - scope.startX;
+                    element.css({
+                        top: scope.offsetY + 'px',
+                        left:  scope.offsetX + 'px'
+                    });
+
+                    event.stopPropagation();
+                }
+
+                function mouseup(event) {
+                    $document.unbind('mousemove', mousemove);
+                    $document.unbind('mouseup', mouseup);
+
+                    event.stopPropagation();
+                }
 
             },
 
