a03c9f9f391b5249b0316598d35c8011ffc22c19
Siegfried Kiermayer <siegfried.kiermayer@mayflower.de>
2014-03-14 13:45:03 +0100

    worked on css for widget

--- web/bundle/dashi/js/Widget.js
+++ web/bundle/dashi/js/Widget.js
@@ -15,8 +15,15 @@ angular.module('Library', []).
                     event.stopPropagation();
                 });
 
-                var innerObj = angular.element('<span>i\'m an widget</span>');
+                var innerObj = angular.element('<span class="name">I\'m a widget</span>');
                 element.append(innerObj);
+
+                element.addClass('widget');
+                element.css('top', event.offsetY);
+                element.css('left', event.offsetX);
+                element.css('width', 300);
+                element.css('height', 100);
+
                 scope.$apply();
 
                 console.log('widget link');
