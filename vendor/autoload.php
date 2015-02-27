f21d79cbd1a3e0707ea853410e969b15f36a1524
Siegfried Kiermayer <siegfried.kiermayer@mayflower.de>
2015-02-27 12:04:22 +0100

    added mongodb dep to s2

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd73f1ce3c8cd7da716f96303e06d0c95::getLoader();
+return ComposerAutoloaderInitfd5c72fd336b4e1c9921537fea7286d5::getLoader();
